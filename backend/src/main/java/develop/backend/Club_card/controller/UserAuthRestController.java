67c413163ded2a87d57e5d38736b59d4af9601ff
dmitrijmrsh <kapustinrudolf@gmail.com>
2024-08-28 23:53:36 +0300

    migrations done

--- backend/src/main/java/develop/backend/Club_card/controller/UserAuthRestController.java
+++ backend/src/main/java/develop/backend/Club_card/controller/UserAuthRestController.java
@@ -3,7 +3,6 @@ package develop.backend.Club_card.controller;
 import develop.backend.Club_card.controller.payload.user.UserLogInPayload;
 import develop.backend.Club_card.controller.payload.user.UserSignUpPayload;
 import develop.backend.Club_card.entity.User;
-import develop.backend.Club_card.service.EmailService;
 import develop.backend.Club_card.service.UserAuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -29,7 +28,6 @@ import java.util.Map;
 public class UserAuthRestController {
 
     private final UserAuthService userAuthService;
-    private final EmailService emailService;
 
     @Operation(
             summary = "Аутентификация пользователя",
@@ -91,7 +89,6 @@ public class UserAuthRestController {
         }
 
         User user = userAuthService.signup(userSignUpPayload);
-        emailService.sendRegistrationCompleteEmail(user.getEmail());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
                 "email", user.getEmail()
